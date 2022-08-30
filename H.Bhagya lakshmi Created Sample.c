175dc5da10fe268abd6a24496001e3a15574bb38
204g1a0519 <112462358+204g1a0519@users.noreply.github.com>
2022-08-30 11:27:26 +0530

    Create H.Bhagya lakshmi Created Sample.c

---
+++ H.Bhagya lakshmi Created Sample.c
@@ -0,0 +1,10 @@
+/* Name Of the Candidate: H.Bhagya lakshmi
+   Roll No: 204G1A0519
+   Title of the Experiment: CNOS LAB
+   Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
+#include <stdio.h>
+    int main(){
+	printf("Hello World\n");
+ }
